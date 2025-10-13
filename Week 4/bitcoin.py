bde7dda0f7bd1b21fb726c2ff6f6bd767f4f9d8d
Mahabubur Rahman <mrmafi32@gmail.com>
2025-10-14 02:03:14 +0600

    week 4

---
+++ Week 4/bitcoin.py
@@ -0,0 +1,21 @@
+import argparse, sys, requests
+
+parser = argparse.ArgumentParser(exit_on_error=False)
+parser.add_argument('number')
+try:
+    args = parser.parse_args()
+    num = float(args.number)
+except argparse.ArgumentError:
+    sys.exit('Missing command-line argument')
+except ValueError:
+    sys.exit('Command-line argument is not a number')
+
+
+try:
+    response = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey=54894533ffcc7e5099f8fec9e85203f130f4030da041cc64091bac9c95504a53")
+    data = response.json()["data"]
+    amount = num * float(data["priceUsd"])
+    print(f"${amount:,.4f}")
+
+except requests.RequestException:
+    sys.exit("Error fetching data")
