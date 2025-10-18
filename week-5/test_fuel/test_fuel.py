21934b0878e524c113c230266db393f140c46daa
Mahabubur Rahman <mrmafi32@gmail.com>
2025-10-18 23:40:55 +0600

    test_fuel

--- week-5/test_fuel/test_fuel.py
+++ week-5/test_fuel/test_fuel.py
@@ -10,10 +10,11 @@ def test_gauge():
     assert gauge(50) == "50%"
 
 def test_convert():
-    assert convert(3/4) == 75
-    assert convert(1/3) == 33
-    assert convert(0/100) == 0
-    assert convert(100/100) == 100
+    assert convert("3/4") == 75
+    assert convert("1/3") == 33
+    assert convert("0/100") == 0
+    assert convert("100/100") == 100
+
     with pytest.raises(ZeroDivisionError):
         convert("100/0")
     with pytest.raises(ValueError):
@@ -24,4 +25,5 @@ def test_convert():
         convert("3/4.5")
     with pytest.raises(ValueError):
         convert("-3/4")
+
 
