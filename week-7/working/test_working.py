7793ae8edb40046846ae474507a3a3f6ecd6b2c7
Mahabubur Rahman <mrmafi32@gmail.com>
2025-11-23 23:55:52 +0600

    working

--- week-7/working/test_working.py
+++ week-7/working/test_working.py
@@ -5,14 +5,21 @@ def test_valid_time():
     assert convert("9 AM to 5 PM") == "09:00 to 17:00"
     assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
     assert convert("10 AM to 8:50 PM") == "10:00 to 20:50"
-    # assert convert("") == ""
+
     assert convert("10:30 PM to 8 AM") == "22:30 to 08:00"
 
 
-def text_invalid_time():
+def test_invalid_time():
     invalid_time = ["9:60 AM to 5:60 PM",
                     "9 AM - 5 PM",
-                    "09:00 AM - 17:00 PM"]
+                    "09:00 AM - 17:00 PM",
+                    "9AM to 5PM",
+                    "9AM - 5PM",
+                    "13 AM to 5 PM",
+                    "0 AM to 5 PM",
+                    "9 AM to 5:70 PM",
+                    "9 AM until 5 PM",
+                    ]
 
     for t in invalid_time:
         with pytest.raises(ValueError):
--- week-7/working/working.py
+++ week-7/working/working.py
@@ -6,11 +6,11 @@ def main():
 
 
 def convert(s):
-    pattern = r'^(\d{1,2})(?:[:\.]?(\d{2}))?\s*(AM|PM)\s*TO\s*(\d{1,2})(?:[:\.]?(\d{2}))?\s*(AM|PM)$'
+    pattern = r'^(\d{1,2})(?:[:\.]?(\d{2}))?\s+(AM|PM)\s*TO\s*(\d{1,2})(?:[:\.]?(\d{2}))?\s+(AM|PM)$'
     check = re.match(pattern, s.strip().upper())
     if not check:
         raise ValueError('Invalid Format')
-    
+
     hour1 = check.group(1)
     minute1 = 0 if check.group(2) == None else check.group(2)
     period1 = check.group(3)
@@ -24,12 +24,10 @@ def convert(s):
         if not 0<=m<=59:
             raise ValueError('Invalid Minutes')
         if p == 'AM':
-            if h == 12:
-                return f'{0:02d}:{m:02d}'
             return f'{h % 12:02d}:{m:02d}'
         else:
-            return f'{h %12 +12:02d}:{m:02d}'
-    
+            return f'{h % 12 +12:02d}:{m:02d}'
+
     return f"{format(int(hour1), int(minute1), period1)} to {format(int(hour2), int(minute2), period2)}"
 
 if __name__ == "__main__":
