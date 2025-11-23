592ac7ce2db95b26ec24ed25c21c96dcd99e9f49
Mahabubur Rahman <mrmafi32@gmail.com>
2025-11-23 23:56:39 +0600

    week-8-check

--- week-7/working/test_working.py
+++ week-7/working/test_working.py
@@ -5,7 +5,6 @@ def test_valid_time():
     assert convert("9 AM to 5 PM") == "09:00 to 17:00"
     assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
     assert convert("10 AM to 8:50 PM") == "10:00 to 20:50"
-
     assert convert("10:30 PM to 8 AM") == "22:30 to 08:00"
 
 
